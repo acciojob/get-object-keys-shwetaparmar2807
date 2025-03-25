2be9628e72ea2d13204f303be68a198e3a2fa75f
shwetaparmar2807 <shwetaparmar2807@gmail.com>
2025-03-25 12:55:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const student = {
+  name: "John",
+};
+
+function getKeys(obj) {
+  return Object.keys(obj);
+}
+
+console.log(getKeys(student)); 
+
+console.log(getKeys({ name: "Alice", age: 25, city: "New York" })); // Output: ["name", "age", "city"]
+console.log(getKeys({ country: "USA" })); 
+console.log(getKeys({}));
